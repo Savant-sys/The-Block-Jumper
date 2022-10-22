c4743e05d098835701597ecbafc0a2a5ba696672
EvanJHolscher <70182638+EvanJHolscher@users.noreply.github.com>
2022-10-22 10:31:41 -0700

    Update Share.cs

--- BlockJumper/Assets/Share.cs
+++ BlockJumper/Assets/Share.cs
@@ -17,19 +17,18 @@ public class Share : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
 
-        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        ss.Apply();
+        Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenshot.Apply();
 
         string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
-        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+        File.WriteAllBytes(filePath, screenshot.EncodeToPNG());
 
         // To avoid memory leaks
-        Destroy(ss);
+        Destroy(screenshot);
 
         new NativeShare().AddFile(filePath)
-            .SetSubject("Block Jumper").SetText(shareMessage).SetUrl("https://github.com/yasirkula/UnityNativeShare")
-            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .SetSubject("Block Jumper").SetText(shareMessage))
             .Share();
 
     }
