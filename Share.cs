5e4220d5e1ae01a77325ec8105f6ff92d75799de
EvanJHolscher <70182638+EvanJHolscher@users.noreply.github.com>
2022-11-05 09:19:55 -0700

    Update Share.cs

--- Share.cs
+++ Share.cs
@@ -7,9 +7,9 @@ public class Share : MonoBehaviour
 {
     private string shareMessage;
 
-    public void ClickShareButton()
+    public void ShareButton()
     {
-        int score = 10;
+        int score = 10; // Temporary Score before added score
         shareMessage = "I can't believe I climbed " + score.ToString() + " feet in BlockJumper!";
         StartCoroutine(TakeScreenshotAndShare());
     }
@@ -17,19 +17,14 @@ public class Share : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
 
-        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        ss.Apply();
+        Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenshot.Apply();
 
-        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
-        File.WriteAllBytes(filePath, ss.EncodeToPNG());
-
-        // To avoid memory leaks
-        Destroy(ss);
+        Destroy(screenshot);
 
         new NativeShare().AddFile(filePath)
-            .SetSubject("Block Jumper").SetText(shareMessage).SetUrl("https://github.com/yasirkula/UnityNativeShare")
-            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .SetSubject("Block Jumper").SetText(shareMessage))
             .Share();
 
     }
